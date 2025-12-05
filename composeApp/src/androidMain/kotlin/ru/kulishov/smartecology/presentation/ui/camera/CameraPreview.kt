458fe16512e16d98cb14824bda167bc292a8d2d5
Ilya Kulishov <DoD.mail@yandex.ru>
2025-12-06 00:36:17 +0300

    takePhoto (not tested)

--- composeApp/src/androidMain/kotlin/ru/kulishov/smartecology/presentation/ui/camera/CameraPreview.kt
+++ composeApp/src/androidMain/kotlin/ru/kulishov/smartecology/presentation/ui/camera/CameraPreview.kt
@@ -1,9 +1,13 @@
 package ru.kulishov.smartecology.presentation.ui.camera
 
+import android.content.Context
 import android.net.Uri
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
@@ -19,10 +23,15 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.LocalLifecycleOwner
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Base64
+import java.util.Date
+import java.util.Locale
 import java.util.concurrent.Executors
 
 @Composable
-fun CameraPreview(){
+fun CameraPreview(shot: Boolean, onSaved:(String)-> Unit){
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
 
@@ -81,13 +90,15 @@ fun CameraPreview(){
                         )
 
 
-
                     } catch (e: Exception) {
                         Log.e("CameraPreview", "Error starting camera", e)
                     }
                 }, ContextCompat.getMainExecutor(context))
             }
         )
+    }
+    if(shot){
+        takePhoto(context,imageCapture, onSaved = { onSaved(it) })
     }
 
     DisposableEffect(Unit) {
@@ -105,4 +116,37 @@ fun CameraPreview(){
             }, ContextCompat.getMainExecutor(context))
         }
     }
+}
+
+private fun takePhoto(
+    context: Context,
+    imageCapture: ImageCapture?,
+    onSaved: (String)-> Unit
+) {
+    val capture = imageCapture ?: return
+
+    val photoFile = File(
+        context.externalMediaDirs.firstOrNull(),
+        "${SimpleDateFormat("yyyyMMdd-HHmmss", Locale.US).format(Date())}.jpg"
+    )
+
+    val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+    capture.takePicture(
+        outputOptions,
+        ContextCompat.getMainExecutor(context),
+        object : ImageCapture.OnImageSavedCallback {
+            @RequiresApi(Build.VERSION_CODES.O)
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                val imageFile = photoFile
+                 val imageBytes = imageFile.readBytes()
+                val base64Image = Base64.getEncoder().encodeToString(imageBytes)
+                onSaved(base64Image)
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                Log.e("Camera", "Error taking photo", exception)
+            }
+        }
+    )
 }
